8c1e7b82f06713fed6f8925c88daf84958c5196d
ramon romero <ramonromero150403@gmail.com>
2023-12-18 16:34:32 -0300

    PreEntrega2 Ramon Romero

--- proyecto-js/js/index.js
+++ proyecto-js/js/index.js
@@ -1,11 +1,51 @@
 
-//NO UTILIZAR LAS SIGUIENTES PALABRAS
-//break, case, catch, continue, default, let
-//delete, do, else, finally, for, function, if, in, instanceof, new, return, switch, this, throw, try, typeof, var, void, while, with, y varias más
 
 let usuario = prompt ("Ingrese su nombre de usuario");
 
-// Algoritmo con condicional
+// Controlador de stock
+const ropaVeranoHombres = [
+    {nombre: "Remera Negra Lisa", precio: 1500},
+    {nombre: "Remera Blanca Lisa", precio:1500},
+    {nombre: "Remera Sin mangas con estampado", precio: 2000},
+    {nombre: "Remera con Mangas con estampado", precio: 2000},
+]
+
+const ropaInviernoHombres = [
+    {nombre: "Remera Mangas Larga Negra Lisa", precio: 3000},
+    {nombre: "Remera Mangas Larga Blanca Lisa", precio:3000},
+    {nombre: "Remera Mangas Larga con estampado", precio: 3500},
+    {nombre: "Remera Mangas Larga con estampado dif", precio:3500},
+]
+
+const ropaVeranoMujeres = [
+    {nombre: "Remera Negra Lisa", precio: 1500},
+    {nombre: "Remera Blanca Lisa", precio:1500},
+    {nombre: "Remera Sin mangas con estampado", precio: 2000},
+    {nombre: "Remera con Mangas con estampado", precio: 2000},
+]
+
+const ropaInviernoMujeres = [
+    {nombre: "Remera Mangas Larga Negra Lisa", precio: 3000},
+    {nombre: "Remera Mangas Larga Blanca Lisa", precio:3000},
+    {nombre: "Remera Mangas Larga con estampado", precio: 3500},
+    {nombre: "Remera Mangas Larga con estampado dif", precio:3500},
+]
+
+const ropaVeranoNiños = [
+    {nombre: "Remera Negra Lisa", precio: 1500},
+    {nombre: "Remera Blanca Lisa", precio:1500},
+    {nombre: "Remera Sin mangas con estampado", precio: 2000},
+    {nombre: "Remera con Mangas con estampado", precio: 2000},
+]
+
+const ropaInviernoNiños = [
+    {nombre: "Remera Mangas Larga Negra Lisa", precio: 3000},
+    {nombre: "Remera Mangas Larga Blanca Lisa", precio:3000},
+    {nombre: "Remera Mangas Larga con estampado", precio: 3500},
+    {nombre: "Remera Mangas Larga con estampado dif", precio:3500},
+]
+
+const stock = ropaVeranoHombres.map ()
 
 
 
@@ -16,7 +56,7 @@ let usuario = prompt ("Ingrese su nombre de usuario");
 
 
 
-
+// Controlador de Stock
 
 // Simulador interactivo
 
@@ -30,8 +70,8 @@ while (menu) {
         case 1: // Caso Hombre
             let opcion2 = parseInt(prompt("Selecciona la ropa segun la estacion: \n1.Ropa de Verano\n2.Ropa de Invierno\n3.Salir"));
             switch (opcion2){
-                case 1:
-                    alert("Ahora veras remeras, bermudas y calzado de verano");
+                case 1: 
+                alert("Ahora veras remeras, bermudas y calzado de verano");
                     break;
                 case 2:
                     alert("Ahora veras Buzos, reneras mangas largas, pantalones y calzado de invierno");
@@ -44,11 +84,14 @@ while (menu) {
                     alert("Opcion no valida");
                     break;
             }
+            break;
+
+
         case 2: // Caso Mujer
         let opcion3 = parseInt(prompt("Selecciona la ropa segun la estacion: \n1.Ropa de Verano\n2.Ropa de Invierno\n3.Salir"));
         switch (opcion3){
             case 1:
-                    alert("Ahora veras remeras, bermudas y calzado de verano");
+                alert("Ahora veras remeras, bermudas y calzado de verano");
                     break;
                 case 2:
                     alert("Ahora veras Buzos, reneras mangas largas, pantalones y calzado de invierno");
@@ -61,6 +104,9 @@ while (menu) {
                     alert("Opcion no valida");
                     break;
         }
+        break;
+
+
         case 3: // Caso Niños y niñas
         let opcion4 = parseInt(prompt("Selecciona la ropa segun la estacion: \n1.Ropa de Verano\n2.Ropa de Invierno\n3.Salir"));
         switch (opcion4){
@@ -78,6 +124,9 @@ while (menu) {
                     alert("Opcion no valida");
                     break;
         } 
+        break;
+
+
         case 4:
             menu = false
                 alert("Gracias por ver, esperamos pronto su regreso");
@@ -90,3 +139,44 @@ while (menu) {
   }
 
   // Simulador Interactivo
+
+  // CARRITO INTERACTIVO
+
+const carritoCompras = () => {
+    let carrito = true;
+
+    while (carrito) {
+        let prendas = parseInt(
+            prompt(`
+            
+            Que prendas quieres ver hoy?
+            1- Remeras
+            2- Bermudas
+            3- Calzado
+            4- Volver
+            `
+            )
+        );
+        switch (carrito) {
+            case 1:
+
+            break;
+
+            case 2:
+
+            break;
+
+            case 3:
+
+            break;
+
+            case 4:
+                menu();
+            break;
+
+            default:
+                alert("Ingrese una opcion valida");
+                break;
+        }
+    }
+}
